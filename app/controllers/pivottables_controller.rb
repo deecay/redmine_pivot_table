1a61ebf242703f8de507a16a39486488f0a2b79e
deecay <daiju.kito@gmail.com>
2016-01-07 16:30:47 +0900

    New Activities table.

--- app/controllers/pivottables_controller.rb
+++ app/controllers/pivottables_controller.rb
@@ -18,30 +18,37 @@ class PivottablesController < ApplicationController
     @language_js = "pivot." + current_language.to_s + ".js"
     @statuses = IssueStatus.sorted.collect{|s| [s.name] }
 
-    retrieve_query
-    @query.project = @project
-
-    # Exclude description
-    index = nil
-    @query.available_columns.each_with_index {|column, i| index = i if column.name == :description}
-    @query.available_columns.delete_at(index)
-
-    if (params[:closed] == "1")
-        @query.add_filter("status_id", "*", [''])
-    else
-        @query.add_filter("status_id", "o", [''])
-    end
-
     @rows = params[:rows] ? params[:rows].split(",") : nil
     @cols = params[:cols] ? params[:cols].split(",") : nil
     @aggregatorName = params[:aggregatorName]
     @vals = params[:vals] ? params[:vals].split(",") : nil
     @rendererName = params[:rendererName]
 
-    @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version],
+    @table = params[:table]
+
+    if (@table == "activity")
+      @days = Setting.activity_days_default.to_i
+      @with_subprojects = Setting.display_subprojects_issues?
+      @activity = Redmine::Activity::Fetcher.new(User.current, :project => @project,
+                                                               :with_subprojects => @with_subprojects)
+      @events = @activity.events(Date.today - @days, Date.today + 1)
+    else
+      retrieve_query
+      @query.project = @project
+
+      # Exclude description
+      @query.available_columns.delete_if { |querycolumn| querycolumn.name == :description }
+
+      if (params[:closed] == "1")
+          @query.add_filter("status_id", "*", [''])
+      else
+          @query.add_filter("status_id", "o", [''])
+      end
+
+      @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version],
                               :offset => 0,
                               :limit => 1000)
-
+    end
   end
 
 end
--- app/helpers/pivottables_helper.rb
+++ app/helpers/pivottables_helper.rb
@@ -1,8 +1,51 @@
 module PivottablesHelper
   def pv_caption(colname)
-    col = @query.available_columns.find(){ |x| x.name == colname }
-    unless col.nil? then
-      col.caption
+    if @query
+      col = @query.available_columns.find(){ |x| x.name == colname }
+      unless col.nil? then
+        col.caption
+      end
     end
+  end
+
+  def parse_events(events)
+
+    result_list = Array.new
+
+    events.each{|e|
+
+      if e.kind_of?(TimeEntry) then
+        hours = e.hours
+
+      elsif e.kind_of?(Changeset) then
+        repository = e.repository
+
+      end
+
+      activity = l("label_#{e.event_type.singularize.gsub(/-/, '_')}_plural")
+      author = link_to_user(e.event_author) if e.respond_to?(:event_author)
+      title = link_to(format_activity_title(e.event_title), e.event_url)
+
+      time = e.event_datetime
+      zone = User.current.time_zone
+      local = zone ? time.in_time_zone(zone) : (time.utc? ? time.localtime : time)
+
+      result_list.push({l("field_activity") => activity,
+                        ID:e.id, 
+                        l("field_author") => author, 
+                        l("field_title") => title,
+                        l("field_created_on") => local,
+                        l("field_created_on")+"(y)" => local.year,
+                        l("field_created_on")+"(m)" => local.month,
+                        l("field_created_on")+"(d)" => local.mday,
+                        l("field_created_on")+"(U)" => local.strftime("%U"),
+                        l("field_created_on")+"(w)" => local.wday,
+                        l("field_created_on")+"(h)" => local.hour,
+                        })
+
+    }
+
+    result_list
+
   end
 end
