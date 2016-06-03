e4909dc4d3b937ae727e5b770fdc063e8ac3815c
deecay <daiju.kito@gmail.com>
2016-06-03 11:24:49 +0900

    Added basic save function

--- app/controllers/pivottables_controller.rb
+++ app/controllers/pivottables_controller.rb
@@ -2,6 +2,7 @@ class PivottablesController < ApplicationController
   unloadable
   before_filter :find_project, :authorize, :only => :index
 
+  helper :issues
   helper :queries
   include QueriesHelper
   helper :repositories
@@ -19,6 +20,18 @@ class PivottablesController < ApplicationController
     @language_js = "pivot." + current_language.to_s + ".js"
     @statuses = IssueStatus.sorted.collect{|s| [s.name] }
 
+    if (params[:query_id])
+      @query = Query.find(params[:query_id])
+    end
+
+    retrieve_query
+    if (!@query.new_record? && params[:table] != "activity")
+      pivot_config = @query.options[:pivot_config]
+      pivot_config.each do |k, v|
+        params[k] = v
+      end
+    end
+
     @rows = params[:rows] ? params[:rows].split(",") : nil
     @cols = params[:cols] ? params[:cols].split(",") : nil
     @aggregatorName = params[:aggregatorName]
@@ -27,14 +40,19 @@ class PivottablesController < ApplicationController
 
     @table = params[:table]
 
-    if (@table == "activity")
+    params.delete("rows")
+    params.delete("cols")
+    params.delete("aggregatorName")
+    params.delete("attrdropdown")
+    params.delete("rendererName")
+
+    if (params[:table] == "activity")
       @days = Setting.activity_days_default.to_i
       @with_subprojects = Setting.display_subprojects_issues?
       @activity = Redmine::Activity::Fetcher.new(User.current, :project => @project,
                                                                :with_subprojects => @with_subprojects)
       @events = @activity.events(Date.today - @days, Date.today + 1)
     else
-      retrieve_query
       @query.project = @project
 
       # Exclude description
@@ -55,4 +73,50 @@ class PivottablesController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 
+  def new
+    @project = Project.find(params[:project_id])
+    retrieve_query
+    @query.project = @project
+    @query.name = ""
+    @query.options[:pivot_config] = { :table => params[:table],
+               :rows => params[:rows],
+               :cols => params[:cols],
+               :rendererName => params[:rendererName],
+               :aggregatorName => params[:aggregatorName],
+               :attrdropdown => params[:attrdropdown] }
+  end
+
+  def save
+    logger.info "EXECUTING SAVE"
+    @project = Project.find(params[:project_id])
+    retrieve_query
+
+    @query.user = User.current
+    @query.project = params[:query_is_for_all] ? nil : @project
+    @query.build_from_params(params)
+    @query.column_names = nil if params[:default_columns]
+    @query.sort_criteria = params[:query] && params[:query][:sort_criteria]
+    @query.name = params[:query] && params[:query][:name]
+    if User.current.allowed_to?(:manage_public_queries, @query.project) || User.current.admin?
+      @query.visibility = (params[:query] && params[:query][:visibility]) || IssueQuery::VISIBILITY_PRIVATE
+      @query.role_ids = params[:query] && params[:query][:role_ids]
+    else
+      @query.visibility = IssueQuery::VISIBILITY_PRIVATE
+    end
+
+    config = params[:query][:options][:pivot_config]
+    @query.options[:pivot_config] = { :table => config[:table],
+               :rows => config[:rows],
+               :cols => config[:cols],
+               :rendererName => config[:rendererName],
+               :aggregatorName => config[:aggregatorName],
+               :attrdropdown => config[:attrdropdown] }
+
+    if @query.save
+      flash[:notice] = l(:notice_successful_create)
+      redirect_to project_pivottables_path(@project)
+    else
+      render :action => 'new', :layout => !request.xhr?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 get '/pivottables' => 'pivottables#index'
+get '/projects/:project_id/pivottables/save' => 'pivottables#save'
+post '/projects/:project_id/pivottables/save' => 'pivottables#save'
+get '/projects/:project_id/pivottables/new' => 'pivottables#new'
 
 resources :projects do
   resources :pivottables, :only => [:index]
